d013521d0c389f226d47da979c1baea5353c9433
Taikatou <conorstephens@gmail.com>
2017-09-15 12:28:04 +0100

    big update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject target;
+    public float followAhead;
+    public float smoothing;
 
 	// Use this for initialization
 	private void Start ()
@@ -16,6 +18,12 @@ public class CameraController : MonoBehaviour
 	private void Update ()
     {
         Vector3 position = target.transform.position;
-        transform.position = new Vector3(position.x, transform.position.y, transform.position.z);
+        bool movingLeft = target.transform.localScale.x > 0f;
+        float localFollowAhead = movingLeft? followAhead : -followAhead;
+        Vector3 targetPosition = new Vector3(position.x + localFollowAhead,
+                                             transform.position.y,
+                                             transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
 	}
 }
