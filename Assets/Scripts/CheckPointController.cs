14e97cefd3c816db32ee3d555020dd3c72736b4d
Taikatou <13123947@studentmail.ul.ie>
2017-09-17 00:01:40 +0100

    Update Two

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    public Sprite flagClosed;
+    public Sprite flagOpen;
+    public bool traceUp = true;
+    public bool traceDown = false;
+    public bool openOnce = true;
+
+    private bool _open = false;
+
+    private SpriteRenderer spriteRenderer
+    {
+        get
+        {
+            return GetComponent<SpriteRenderer>();
+        }
+    }
+
+    void Update()
+    {
+        if(!_open || !openOnce)
+        {
+            ConditionTrace(traceUp, Vector2.up);
+            ConditionTrace(traceDown, -Vector2.up);
+        }
+    }
+
+    void ConditionTrace(bool condition, Vector2 direction)
+    {
+        if(condition)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+            if (hit.collider != null)
+            {
+                CheckCollision(hit);
+            }
+        }
+    }
+
+    void CheckCollision(RaycastHit2D hit)
+    {
+        if (hit.collider.tag == "Player")
+        {
+            _open = !_open;
+            spriteRenderer.sprite = _open? flagOpen : flagClosed;
+        }
+    }
+}
