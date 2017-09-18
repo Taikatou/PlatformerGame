4fff72f1528608cf26024b2284722c82e95a9d9f
Taikatou <conorstephens@gmail.com>
2017-09-18 17:03:13 +0100

    update

--- Assets/Scripts/Player/PlayerKillPlane.cs
+++ Assets/Scripts/Player/PlayerKillPlane.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerKillPlane : KillPlaneDestroyable
+﻿public class PlayerKillPlane : KillPlaneDestroyable
 {
     public override void Kill()
     {
