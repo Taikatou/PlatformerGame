522121c9c90f44861cd18e60b7e2d5751f702fb8
Taikatou <conorstephens@gmail.com>
2017-09-21 15:26:27 +0100

    pudate

---
+++ Assets/Scripts/FollowControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FollowControl : MonoBehaviour
+{
+    public GameObject follower;
+    public GameObject target;
+    public float followAhead;
+    public float smoothing;
+
+    public bool moveY = false;
+    public bool includeTime = true;
+
+	// Update is called once per frame
+	private void Update ()
+    {
+        bool movingLeft = target.transform.localScale.x > 0f;
+        float localFollowAhead = movingLeft? followAhead : -followAhead;
+        float yMovement = moveY ? target.transform.position.y : follower.transform.position.y;
+        Vector3 targetPosition = new Vector3(target.transform.position.x + localFollowAhead,
+                                             yMovement,
+                                             follower.transform.position.z);
+
+        float smooth = includeTime ? smoothing * Time.deltaTime : smoothing;
+        Debug.Log(smooth);
+        follower.transform.position = Vector3.Lerp(follower.transform.position, targetPosition, smooth);
+	}
+}
