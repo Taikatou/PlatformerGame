f80401d8d41318640bc4ac6dbc5e47b5a18a65b1
Taikatou <conorstephens@gmail.com>
2017-09-22 13:30:28 +0100

    update

---
+++ Assets/Scripts/GameObjects/Trampoline.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float JumpPower = 100;
+
+    public AudioSource jumpSound;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+        if (hit.collider && hit.collider.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpPower);
+            if (jumpSound)
+            {
+                jumpSound.Play();
+            }
+        }
+    }
+}
